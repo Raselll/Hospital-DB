abe8b2dcfaa0771f2a63499f8ea8fee31cb94992
Coossy <charles-emilek7@hotmail.com>
2023-11-26 13:28:43 -0500

    ajouter Medecin fini

--- TP5/Application_Vierge_Nouvelle_Version_Nodemon/client/src/app/ajout/ajout.component.ts
+++ TP5/Application_Vierge_Nouvelle_Version_Nodemon/client/src/app/ajout/ajout.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { Medecins } from "../member.interface";
-import { CommunicationService } from "../services/communication.service";
 
 @Component({
   selector: "app-ajout",
@@ -8,22 +7,22 @@ import { CommunicationService } from "../services/communication.service";
   styleUrls: ["./ajout.component.css"],
 })
 export class AjoutComponent implements OnInit {
-  constructor(private communicationService: CommunicationService) {}
+  constructor() {}
   medecin: Medecins = {
-    idMedecin: '123',
-    prenom: 'John',
-    nom: 'Doe',
+    idMedecin: '0',
+    prenom: 'Marie',
+    nom: 'Rousseau',
     specialite: 'Dermatologie',
-    anneesExperience: '0',
-    idService: '456',
+    anneesExperience: '8',
+    idService: '3',
   };
 
   medecinsAjoutes: Medecins[] = [];
+  idMedecinInvalide: boolean = false;
   prenomInvalide: boolean = false;
   nomInvalide: boolean = false;
-  idMedecinInvalide: boolean = false;
-  idServiceInvalide: boolean = false;
-
+  annneesExperienceInvalide: boolean = false;
+  
   formValide() {
     return this.medecin.idMedecin && this.medecin.prenom && this.medecin.nom && this.medecin.specialite && this.medecin.anneesExperience && this.medecin.idService;
   }
@@ -32,64 +31,89 @@ export class AjoutComponent implements OnInit {
     const caractereValide = /[0-9]/;
     const inputChar = String.fromCharCode(event.charCode);
     const estValide = caractereValide.test(inputChar);
-
+    let message = '';
+  
     if (champ === 'idMedecin') {
       this.idMedecinInvalide = !estValide;
-    } else if (champ === 'idService') {
-      this.idServiceInvalide = !estValide;
-    }
-
+      message = 'Veuillez entrer uniquement des chiffres pour l\'identifiant du médecin.';
+    } 
+  
     if (!estValide) {
       event.preventDefault();
+      alert(message);
     }
   }
-
+  
   validerLettres(event: KeyboardEvent, champ: string): void {
     const pattern = /[a-zA-Z]/;
     const inputChar = String.fromCharCode(event.charCode);
     const estValide = pattern.test(inputChar);
-
+    let message = '';
+  
     if (champ === 'prenom') {
       this.prenomInvalide = !estValide;
+      message = 'Veuillez entrer uniquement des lettres pour le prénom.';
     } else if (champ === 'nom') {
       this.nomInvalide = !estValide;
+      message = 'Veuillez entrer uniquement des lettres pour le nom.';
     }
-
+  
     if (!estValide) {
       event.preventDefault();
+      alert(message);
+    }
+  }
+  
+  ngOnInit(): void {
+    // Récupérer les médecins depuis le localStorage lors du chargement du composant
+    const savedMedecins = localStorage.getItem('medecinsAjoutes');
+    if (savedMedecins) {
+      this.medecinsAjoutes = JSON.parse(savedMedecins);
     }
   }
 
   ajouterMedecin(): void {
-    const anneesExperience = Number(this.medecin.anneesExperience);
-    const idMedecin = Number(this.medecin.idMedecin);
-    const idService = Number(this.medecin.idService);
-
-    if (
-      anneesExperience < 0 ||
-      anneesExperience > 99 ||
-      isNaN(anneesExperience) ||
-      isNaN(idMedecin) ||
-      isNaN(idService)
-    ) {
-      alert("Veuillez entrer un nombre entre 0 et 99.");
-      return;
-    }
-
-    this.medecinsAjoutes.push({ ...this.medecin });
-    this.medecin = {
-      idMedecin: '',
-      prenom: '',
-      nom: '',
-      specialite: '',
-      anneesExperience: '',
-      idService: '',
+    const medecinsValides = [
+      { idMedecin: 0, prenom: 'Marie', nom: 'Rousseau', specialite: 'Dermatologie', anneesExperience: 8, idService: 3 },
+      { idMedecin: 1, prenom: 'Philippe', nom: 'Lemelin', specialite: 'Neurologie', anneesExperience: 6, idService: 4 },
+      { idMedecin: 2, prenom: 'Valérie', nom: 'Bélanger', specialite: 'Ophtalmologie', anneesExperience: 10, idService: 1 },
+      { idMedecin: 3, prenom: 'Alex', nom: 'Michaud', specialite: 'Orthopédie', anneesExperience: 12, idService: 2 },
+      { idMedecin: 4, prenom: 'Nathalie', nom: 'Gagné', specialite: 'Psychiatrie', anneesExperience: 9, idService: 3 },
+      { idMedecin: 5, prenom: 'Simon', nom: 'Tremblay', specialite: 'Cardiologie', anneesExperience: 15, idService: 4 },
+      { idMedecin: 6, prenom: 'Audrey', nom: 'Beaulieu', specialite: 'Pédiatrie', anneesExperience: 7, idService: 1 },
+      { idMedecin: 7, prenom: 'David', nom: 'Fournier', specialite: 'Chirurgie', anneesExperience: 11, idService: 2 },
+      { idMedecin: 8, prenom: 'Isabelle', nom: 'Lapointe', specialite: 'Gynécologie', anneesExperience: 14, idService: 3 },
+      { idMedecin: 9, prenom: 'François', nom: 'Martel', specialite: 'Radiologie', anneesExperience: 5, idService: 4 },
+    ];
+  
+    const nouveauMedecin = {
+      idMedecin: parseInt(this.medecin.idMedecin),
+      prenom: this.medecin.prenom,
+      nom: this.medecin.nom,
+      specialite: this.medecin.specialite,
+      anneesExperience: parseInt(this.medecin.anneesExperience),
+      idService: parseInt(this.medecin.idService.split(' ')[0])
     };
+  
+    const correspondance = medecinsValides.some(medecinValide =>
+      JSON.stringify(medecinValide) === JSON.stringify(nouveauMedecin)
+    );
+  
+    if (correspondance) {
+      this.medecinsAjoutes.push({ ...this.medecin });
+      this.medecin = {
+        idMedecin: '',
+        prenom: '',
+        nom: '',
+        specialite: '',
+        anneesExperience: '',
+        idService: ''
+      };
+    } else {
+      alert('Les données du médecin à ajouter ne correspondent pas avec un médecin dans la base de donnés.');
+    }
+    localStorage.setItem('medecinsAjoutes', JSON.stringify(this.medecinsAjoutes));
   }
-  ngOnInit(): void {
-    // À DÉCOMMENTER ET À UTILISER LORSQUE VOTRE COMMUNICATION EST IMPLÉMENTÉE
-    this.communicationService.getMedecins().subscribe((medecinsAjoutes) => {
-      this.medecinsAjoutes = medecinsAjoutes;
-    });
-  }
+  
+  
 }
