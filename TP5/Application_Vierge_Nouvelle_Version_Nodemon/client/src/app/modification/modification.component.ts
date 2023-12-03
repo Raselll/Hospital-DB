6bca5acedd6f07773a2970146cc541ec183a49cc
Coossy <charles-emilek7@hotmail.com>
2023-12-03 14:50:25 -0500

    TP5 Compléter

--- TP5/Application_Vierge_Nouvelle_Version_Nodemon/client/src/app/modification/modification.component.ts
+++ TP5/Application_Vierge_Nouvelle_Version_Nodemon/client/src/app/modification/modification.component.ts
@@ -11,7 +11,18 @@ export class ModificationComponent implements OnInit {
   medecins: Medecins[] = [];
   medecinsAjoutes: Medecins[] = [];
   isEditing: { [key: string]: boolean } = {};
-
+  numerosService: number[] = Array.from({ length: 10 }, (_, i) => i); 
+  specialites: string[] = ["Dermatologie", 
+                           "Neurologie", 
+                           "Ophtalmologie", 
+                           "Orthopédie", 
+                           "Psychiatrie", 
+                           "Cardiologie", 
+                           "Pédiatrie", 
+                           "Chirurgie", 
+                           "Gynécologie", 
+                           "Radiologie"];
+                           
   constructor(private communicationService: CommunicationService) { }
 
   ngOnInit(): void {
@@ -32,13 +43,25 @@ export class ModificationComponent implements OnInit {
     this.isEditing[id] = !this.isEditing[id];
   }
 
+  validateLetters(input: string): boolean {
+    const lettersPattern = /[a-zA-ZàâæäéèêëîïôœöùûüÿçÀÂÆÄÉÈÊËÎÏÔŒÖÙÛÜŸÇ]/;
+    return lettersPattern.test(input);
+  }
+
   saveChanges(medecin: Medecins): void {
     if (this.isEditing[medecin.idMedecin]) {
       const index = this.medecinsAjoutes.findIndex(m => m.idMedecin === medecin.idMedecin);
       if (index !== -1) {
-        this.medecinsAjoutes[index].prenom = medecin.prenom;
-        this.medecinsAjoutes[index].nom = medecin.nom;
-        this.medecinsAjoutes[index].specialite = medecin.specialite;
+        if (!this.validateLetters(medecin.prenom) || !this.validateLetters(medecin.nom)) {
+          alert('Le prénom et le nom doivent contenir uniquement des lettres majuscules ou minuscules.');
+          return; 
+        }
+        
+        const anneesExperienceValue = +medecin.anneesExperience;
+        if (isNaN(anneesExperienceValue) || anneesExperienceValue < 0 || anneesExperienceValue > 99) {
+          alert('Les années d\'expérience doivent être un nombre compris entre 0 et 99.');
+          return; // Arrêter l'exécution si la validation échoue
+        }
         this.medecinsAjoutes[index].anneesExperience = medecin.anneesExperience;
         this.medecinsAjoutes[index].idService = medecin.idService;
 
